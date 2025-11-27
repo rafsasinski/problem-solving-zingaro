1730ee456d9338a7019191f50bd4a6674542d891
rafsasinski <rafsasinski@gmail.com>
2025-11-27 14:28:25 +0000

    dmopc15c7p2: Word Count

---
+++ dmopc15c7p2_Not_a_Wall_of_Text/word_count.py
@@ -0,0 +1,12 @@
+# https://dmoj.ca/problem/dmopc15c7p2
+# SOLUTION 01
+line = input()
+word_count = line.count(" ") + 1
+print(word_count)
+
+
+# SOLUTION 02:
+#line = input()
+#line = line.split(" ")
+#word_count = len(line)
+#print(word_count)
