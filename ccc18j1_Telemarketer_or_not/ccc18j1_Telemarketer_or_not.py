b8fb4c1e27568bab72e0aee140c6fc12df1f9804
rafsasinski <rafsasinski@gmail.com>
2025-11-28 15:24:58 +0000

    ccc18j1: Telemarketer or not - enhance code

--- ccc18j1_Telemarketer_or_not/ccc18j1_Telemarketer_or_not.py
+++ ccc18j1_Telemarketer_or_not/ccc18j1_Telemarketer_or_not.py
@@ -7,9 +7,7 @@ number.append(int(input()))
 number.append(int(input()))
 number.append(int(input()))
 
-if ((number[0] == 8 or number[0] == 9) and
-    (number[3] == 8 or number[3] == 9) and
-    (number[1] == number[2])):
+if number[0] in (8, 9) and number[3] in (8, 9) and number[1] == number[2]:
     print("ignore")
 else:
     print("answer")
