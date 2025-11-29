7035208347f86ef7dab9fe92e0b0277a7ddd4695
rafsasinski <rafsasinski@gmail.com>
2025-11-29 17:03:11 +0000

    ch02: ccc15j1: Special Day

---
+++ ch02/ccc15j1_Special_Day/special_day.py
@@ -0,0 +1,11 @@
+# https://dmoj.ca/problem/Speci
+
+month = int(input())
+day = int(input())
+
+if month < 2 or (month == 2 and day < 18):
+    print("Before")
+elif month == 2 or day == 18:
+    print("Special")
+else:
+    print("After")
