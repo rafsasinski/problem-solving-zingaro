ebf88fe74f5b11ad07600e38fadf823307b34d9d
rafsasinski <rafsasinski@gmail.com>
2025-12-02 18:30:20 +0000

    ch03: wc17c3j3: Uncrackable password - add check to avoid special chars

--- ch03/wc17c3j3_Uncrackable/wc17c3j3_Uncrackable.py
+++ ch03/wc17c3j3_Uncrackable/wc17c3j3_Uncrackable.py
@@ -12,21 +12,24 @@ password = input()
 upper = 0
 lower = 0
 digit = 0
+good = 0
 
 if len(password) >= 8 and len(password) <= 12:
     for ch in password:
         if ch.isupper():
             upper += 1
+            good += 1
         elif ch.islower():
             lower += 1
-        else:
+            good += 1
+        elif ch.isdigit():
             digit += 1
+            good += 1
     
-    if lower >= 3 and upper >= 2 and digit >= 1:
+    if (good == len(password) and
+        lower >= 3 and upper >= 2 and digit >= 1):
         print("Valid")
     else:
         print("Invalid")
 else:
     print("Invalid")
-
-print(status)
