fa3e40257b40625ce5e1e82d88675ff6e05db1a0
rafsasinski <rafsasinski@gmail.com>
2025-12-04 20:22:28 +0000

    ch03: coci16c1p1: Tarifa Data Plan for Pero - Simplyfy the code by removing list

--- ch03/coci16c1p1_Tarifa_Data_Plan_for_Pero/coci16c1p1_Data_Plan_for_Pero.py
+++ ch03/coci16c1p1_Tarifa_Data_Plan_for_Pero/coci16c1p1_Data_Plan_for_Pero.py
@@ -2,11 +2,10 @@
 
 DATA_PLAN = int(input())
 months = int(input())
-carry = []
 next_month = DATA_PLAN
 
-for month in range(0, months):
-    carry.append(DATA_PLAN - int(input()))
-    next_month = next_month +  carry[month]
+for _ in range(0, months):
+    carry = DATA_PLAN - int(input())
+    next_month = next_month + carry
 
 print(next_month)
