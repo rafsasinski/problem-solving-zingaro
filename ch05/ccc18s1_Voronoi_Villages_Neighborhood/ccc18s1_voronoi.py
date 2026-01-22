c39cf6b77b22a381be4f8ad090a5a5210f400369
rafsasinski <rafsasinski@gmail.com>
2026-01-22 16:59:43 +0000

    Removing commens for readability

--- ch05/ccc18s1_Voronoi_Villages_Neighborhood/ccc18s1_voronoi.py
+++ ch05/ccc18s1_Voronoi_Villages_Neighborhood/ccc18s1_voronoi.py
@@ -1,35 +1,4 @@
 # https://dmoj.ca/problem/ccc18s1
-'''
-N total_villages in line
-16
-Ignore is right most
-
-0
-Ignore left most
-
-10
-LM = 4
-RM = 15
-(10 - 4)/2 = 6/2 = 3
-(15 - 10)/2 = 5/2 = 2.5
-3 + 2.5 = 5.5
-
-4
-LM = 0
-RM = 10
-(0-4)/2 = 2
-(10-4)/2 = 6/2 = 3
-5
-
-15
-LM = 10
-RM = 16
-(15-10)/2 = 5/2 = 2.5
-(16-15)/2 = 1/2 = 0.5
-3.0
-
-'''
-
 total_villages = int(input())
 villages = []
 
