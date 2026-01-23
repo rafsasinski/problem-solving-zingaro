2ba5d7c24e2b49eca9ee01597beb22b43126562d
rafsasinski <rafsasinski@gmail.com>
2026-01-23 15:21:51 +0000

    ch05: ecoo17r1p1: School Brunch and Trip - solving without tips from book

---
+++ ch05/ecoo17r1p1_School_Brunch_and_Trip/ecoo17r1p1_School_Brunch_and_Trip.py
@@ -0,0 +1,54 @@
+# https://dmoj.ca/problem/ecoo17r1p1
+'''
+13:35:00
+14:30:00
+Y1  12
+Y2  10
+Y3  7
+Y4  5
+
+0.5 Running the brunch
+0.5 Trip
+
+INPUT:
+    cost - Cost of the trip (integer)
+    Y1, Y2, Y3, Y4 - precentages of total number of students (float)
+    N - Totalnumber of students (integer)
+
+THE CATCH!!
+1.8 people is the same as i person
+Any missing or extra people should be removed from or added to
+the group with the highest percentage
+'''
+
+YEAR_CHARGE = [12, 10, 7, 5]
+
+for dataset in range(10):
+    trip_cost               = int(input())
+    attendance_percentages  = [float(data) for data in input().split(" ")]
+    total_students          = int(input())
+
+    attendees = []
+    for percentage in attendance_percentages:
+        attendees.append(int(percentage * total_students))
+
+    # Missing (+int) or Extra (-int) person
+    missing = total_students - sum(attendees)
+
+    if missing > 0:
+        highest_index = 0
+        for i in range(1, len(attendance_percentages)):
+            if attendance_percentages[i] > attendance_percentages[i-1]:
+                highest_index = i
+        
+        attendees[highest_index] += missing
+
+    total_proceeds = 0
+    for i in range(len(attendees)):
+        total_proceeds += (attendees[i] * YEAR_CHARGE[i])
+
+    if (trip_cost <= (int(total_proceeds/2))):
+        print("NO")
+    else:
+        print("YES")
+
