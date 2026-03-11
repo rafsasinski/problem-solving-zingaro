e48dda64c7a5a1b6d54ee734cf7909bdfe7697fc
rafsasinski <rafsasinski@gmail.com>
2026-03-11 15:16:17 +0000

    ch05: wac3p3: Dance Dance Revolution - Add edge cases cover

--- ch05/wac3p3_Dance_Revolution/wac3p3_Dance_Revolution.py
+++ ch05/wac3p3_Dance_Revolution/wac3p3_Dance_Revolution.py
@@ -4,37 +4,37 @@ S = input()
 M = int(input())
 combos = []
 combos_points = []
-combos_count = []
-combos_first = []
 for _ in range(M):
     line = input().split()
     combos.append(line[0])
     combos_points.append(int(line[1]))
-    combos_count.append(0)
-    combos_first.append(-1)
 
-#combos.sort(key=lambda val: len(val[0]), reverse=True)
+scored_points = 0
+i = 0
 
-for c in range(len(combos)):
-    combo = combos[c]
-    i = 0
-    while i < len(S):
+while i < len(S):
+    best_points = 0
+    longest_match = 0
+    for c in range(len(combos)):
         combo_ch_match = 0
+        combo = combos[c]
+        if len(combo) < longest_match:
+            continue
         for j in range(len(combo)):
-            if not i+j >= len(S):
-                if combo[j] == S[i+j]:
+            if not i + j >= len(S):
+                if combo[j] == S[i + j]:
                     combo_ch_match += 1
-                
-                if j == len(combo)-1 and combo_ch_match == len(combo):
-                    combos_count[c] += 1
-                    if combos_first[c] == -1:
-                        combos_first[c] = i
-                    i += j
-        i+=1
+                    if j == len(combo) - 1 and combo_ch_match == len(combo):
+                        longest_match = len(combo)
+                        best_points = combos_points[c]
 
-combined = list(zip(  list(map(lambda c: len(c), combos)), combos_first, combos_count, combos_points, combos))
-combined.sort(key=lambda x: (x[0], -x[1]), reverse=True)
-winner = combined[0]
+    if longest_match > 0:
+        # Combo found move i + len(combo)
+        i += longest_match
+        scored_points += best_points
+    else:
+        # No combo found move 1
+        i += 1
 
-answer = len(S) + winner[3] * winner[2]
+answer = len(S) + scored_points
 print(answer)
