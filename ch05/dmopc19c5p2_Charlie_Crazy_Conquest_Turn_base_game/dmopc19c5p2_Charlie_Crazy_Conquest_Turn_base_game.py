abcff1173939b684267c173ee68986b0206bd3cc
rafsasinski <rafsasinski@gmail.com>
2026-03-16 17:42:00 +0000

    ch05: dmopc19c5p2: Charlie crazy conquest turn based game

---
+++ ch05/dmopc19c5p2_Charlie_Crazy_Conquest_Turn_base_game/dmopc19c5p2_Charlie_Crazy_Conquest_Turn_base_game.py
@@ -0,0 +1,55 @@
+(N, H) = (int(x) for x in input().split())
+
+players_HP = [H, H]
+players_move = [[], []]
+
+for n in range(N*2):
+    line = [m if m in "AD" else int(m) for m in input().split()]
+    if n < N:
+        players_move[0].append(line)
+    else:
+        players_move[1].append(line)
+
+i = 0
+player_id = 0
+lowest_hp = min(players_HP)
+previous_move = ["",0]
+
+while i < N and lowest_hp > 0:
+
+    # Select current move for player
+    move = players_move[player_id][i]
+    move_type = move[0]
+    move_power = move[1]
+
+    # Activate the move
+    if move_type == "A":
+        if previous_move[0] == "D":
+            1+1
+        else:
+            players_HP[player_id ^ 1] -= move_power
+    elif move_type == "D":
+        if previous_move[0] == "D":
+            players_HP[player_id ^ 1] -= previous_move[1]
+
+    # This might be the last round check for self harm
+    if i == N-1 and player_id == 1:
+        if move_type == "D":
+            players_HP[player_id] -= move_power
+
+
+    # Prepare for next turn
+    lowest_hp = min(players_HP)
+    previous_move = move
+    player_id += 1
+
+    if player_id > 1:
+        i += 1
+        player_id = 0
+
+if players_HP[0] <= 0:
+    print("DEFEAT")
+elif players_HP[1] <= 0:
+    print("VICTORY")
+else:
+    print("TIE")
