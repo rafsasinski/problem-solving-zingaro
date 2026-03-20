9efd9cdaf9160a00ba0aafb7c76cb46f5e8bb8a4
rafsasinski <rafsasinski@gmail.com>
2026-03-20 11:52:41 +0000

    ch06: ccc99s1: Card Game - add function for checking high card

--- ch06/ccc99s1_Card_Game/ccc99s1_Card_Game.py
+++ ch06/ccc99s1_Card_Game/ccc99s1_Card_Game.py
@@ -1,10 +1,12 @@
 # https://dmoj.ca/problem/ccc99s1
 
 DECK_SIZE = 52
-HIGH_CARDS = ['ace', 'king', 'queen', 'jack']
-POINTS_MAX = 4
 deck = []
 player_points = [0, 0]
+
+def no_high(lst):
+    high_cards = ['ace', 'king', 'queen', 'jack']
+    return not any(card in high_cards for card in lst)
 
 for i in range(DECK_SIZE):
     deck.append(input())
@@ -14,25 +16,24 @@ for i in range(DECK_SIZE):
     player_id = i%2
 
     remaining = DECK_SIZE - (i + 1)
+    points = 0
 
-    if card in HIGH_CARDS:
-        points = 0
-        if card == "ace" and remaining >= 4:
-            if not any(card in HIGH_CARDS for card in deck[i+1:i+5]):
-                points = POINTS_MAX - HIGH_CARDS.index(card)
-        elif card == "king" and remaining >= 3:
-            if not any(card in HIGH_CARDS for card in deck[i+1:i+4]):
-                points = POINTS_MAX - HIGH_CARDS.index(card)
-        elif card == "queen" and  remaining >= 2:
-            if not any(card in HIGH_CARDS for card in deck[i+1:i+3]):
-                points = POINTS_MAX - HIGH_CARDS.index(card)
-        elif card == "jack" and  remaining >= 1:
-            if not any(card in HIGH_CARDS for card in deck[i+1:i+2]):
-                points = POINTS_MAX - HIGH_CARDS.index(card)
+    if card == "ace" and remaining >= 4:
+        if no_high(deck[i+1:i+5]):
+            points = 4
+    elif card == "king" and remaining >= 3:
+        if no_high(deck[i+1:i+4]):
+            points = 3
+    elif card == "queen" and  remaining >= 2:
+        if no_high(deck[i+1:i+3]):
+            points = 2
+    elif card == "jack" and  remaining >= 1:
+        if no_high(deck[i+1:i+2]):
+            points = 1
 
-        if points > 0:
-            player_points[player_id] += points
-            print(f"Player {"B" if player_id else "A"} scores {points} point(s).")
+    if points > 0:
+        player_points[player_id] += points
+        print(f"Player {"B" if player_id else "A"} scores {points} point(s).")
 
 for i in range(len(player_points)):
     print(f"Player {"B" if i%2 else "A"}: {player_points[i%2]} point(s).")
